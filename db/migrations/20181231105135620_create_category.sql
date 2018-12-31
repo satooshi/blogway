c5f5f0985ca1fa8d14173a8bf64e672eb68f59eb
Kitamura Satoshi <satooshi.jp@gmail.com>
2018-12-31 15:17:40 +0900

    Add index and constraint

--- db/migrations/20181231105135620_create_category.sql
+++ db/migrations/20181231105135620_create_category.sql
@@ -1,15 +1,19 @@
 -- +micrate Up
 CREATE TABLE categories (
   id BIGSERIAL PRIMARY KEY,
-  name VARCHAR,
-  url VARCHAR,
-  parent_id INT,
-  sort INT,
+  name VARCHAR NOT NULL,
+  url VARCHAR NOT NULL,
+  parent_id INT DEFAULT NULL,
+  sort INT NOT NULL DEFAULT 0,
   description VARCHAR,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL
 );
 
+CREATE INDEX index_categories_on_parent_id ON public.categories USING btree (parent_id);
+
+ALTER TABLE public.categories ADD UNIQUE("name");
+ALTER TABLE public.categories ADD UNIQUE("url");
 
 -- +micrate Down
 DROP TABLE IF EXISTS categories;
--- db/migrations/20181231110549614_create_article.sql
+++ db/migrations/20181231110549614_create_article.sql
@@ -1,17 +1,22 @@
 -- +micrate Up
 CREATE TABLE articles (
   id BIGSERIAL PRIMARY KEY,
-  title VARCHAR,
-  markdown TEXT,
-  html TEXT,
-  plain_text TEXT,
-  is_public BOOL,
+  title VARCHAR NOT NULL,
+  markdown TEXT NOT NULL,
+  html TEXT NOT NULL,
+  plain_text TEXT NOT NULL,
+  is_public BOOL NOT NULL DEFAULT false,
   published_at TIMESTAMP,
-  url VARCHAR,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP
+  url VARCHAR NOT NULL,
+  category_id BIGINT,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL
 );
 
+CREATE INDEX index_articles_on_category_id ON public.articles USING btree (category_id);
+
+ALTER TABLE ONLY public.articles
+  ADD CONSTRAINT fk_articles_to_categories_id FOREIGN KEY (category_id) REFERENCES public.categories(id)
 
 -- +micrate Down
 DROP TABLE IF EXISTS articles;
